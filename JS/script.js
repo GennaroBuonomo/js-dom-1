4a3541c1a826f099386522f529aba257477dce78
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-09 17:57:45 +0200

    fix js index

--- JS/script.js
+++ JS/script.js
@@ -2,17 +2,18 @@
 const lampadina = document.getElementById("lampadina");
 const myButton = document.getElementById("myButton");
 
+console.log(lampadina)
 // stato iniziale
 let accesa = false; 
 
 myButton.addEventListener("click", function(){
 if (accesa) {
   // spenta
-  lampadina.src = "white_lamp.png";
+  lampadina.src = './img/white_lamp.png';
   accesa = false;
 } else {
   // accesa
-  lampadina.src = "yellow_lamp.png" 
+  lampadina.src = "./img/yellow_lamp.png";
   accesa = true
 }
 })
