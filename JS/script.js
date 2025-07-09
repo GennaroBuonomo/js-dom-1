164de57082aec3e96a3697f4c72bde63e9e6f828
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-09 17:38:18 +0200

    feat inserimento if e else

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,14 @@ const myButton = document.getElementById("myButton");
 // stato iniziale
 let accesa = false; 
 
+myButton.addEventListener("click", function(){
+if (accesa) {
+  // spenta
+  lampadina.src = "white_lamp.png";
+  accesa = false;
+} else {
+  // accesa
+  lampadina.src = "yellow_lamp.png" 
+  accesa = true
+}
+})
