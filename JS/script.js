c4a42e3d1ab8a290dd7b9f1198ce17dd61806247
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-09 17:29:27 +0200

    feat fine base inizio js con la dichiarazione funzione

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// Dichiarazione funzione
+const lampadina = document.getElementById("lampadina");
+const myButton = document.getElementById("myButton");
+
+// stato iniziale
+let accesa = false; 
+
